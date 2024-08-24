d2b9c23644128c43bf7ed2d343291c8d17b16926
potofgreed1 <131305294+potofgreed1@users.noreply.github.com>
2024-08-23 20:10:40 -0700

    Create firebaseconfig.ts

---
+++ src/firebaseconfig.ts
@@ -0,0 +1,13 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCrZw1nIwHHoglV-L1AcOrcXasDRrFg0A8",
+  authDomain: "raceme-54047.firebaseapp.com",
+  databaseURL: "https://raceme-54047-default-rtdb.firebaseio.com",
+  projectId: "raceme-54047",
+  storageBucket: "raceme-54047.appspot.com",
+  messagingSenderId: "530870651607",
+  appId: "1:530870651607:web:6806b1b42ea85d1d5a7e92",
+  measurementId: "G-S7RNR562MT"
+};
+
+
+export default firebaseConfig;
